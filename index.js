e453e56b83612ddd17fbee1658ebf65a0199fd6d
Catalin George Festila <catafest76@gmail.com>
2023-06-01 14:12:50 +0300

    update example with got.stream

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 import got from 'got';
+import fs from 'node:fs';
+import {pipeline as streamPipeline} from 'node:stream/promises';
+
+const url = 'https://jsonplaceholder.typicode.com/posts/1';
+
+// using got.get
 const getData = async () => {
 	try {
 		const res = await got
-			.get('https://jsonplaceholder.typicode.com/posts/1')
+			.get(url)
 			.json();
 		console.log(res);
 	} catch (err) {
@@ -10,4 +16,27 @@ const getData = async () => {
 	}
 };
 
-getData();
+const data = getData();
+
+// using got.stream
+const stream = got.stream(url)
+  .once('error', error => {
+    console.error(error.response.statusCode);
+  });
+
+const outStream = fs.createWriteStream('posts.json');
+
+stream.on('data', chunk => {
+  outStream.write(chunk);
+});
+
+stream.on('end', async () => {
+  outStream.end();
+  console.log('Receiving data completed.');
+  try {
+    await streamPipeline(stream, outStream);
+    console.log('Data written to posts.json file.');
+  } catch (error) {
+    console.error(error);
+  }
+});
